1aee22b49e5e32e57d61a38b2f49fefa4202c721
Tanjin Prity <tanjin.prity@bison.howard.edu>
2019-12-06 21:19:04 +0000

    Using TSLGenerator for Category-Partition testing.

---
+++ myTest/modify_friendlist.py
@@ -0,0 +1,42 @@
+# these are the test cases to be used when testing the actual code. 
+colName = 'friendlist'
+docName = 'document'
+
+# put in data first before testing modifications to it.
+firestoreStorage.updateField(colName, docName, [('field', ['value'])])
+
+def testCase_1():
+  firestoreStorage.updateField(colName, docName, [])
+  assert firestoreStorage.getFieldValue(colName, docName, 'field') = 'value'
+  
+def testCase_2():
+  firestoreStorage.updateField(colName, docName, [('field', ['newVal'])])
+  assert firestoreStorage.getFieldValue(colName, docName, 'field') = 'newVal'
+
+def testCase_3():
+  try:
+    firestoreStorage.updateField(colName, docName, [('field', ['?#$'])])
+  except Exception:
+    return True
+  
+def testCase_4():
+  try:
+    firestoreStorage.updateField(colName, docName, [('field', [''])])
+  except Exception:
+    return True
+
+def testCase_5():
+  firestoreStorage.updateField(colName, docName, [('field', ['val1', 'val2'])])
+  assert firestoreStorage.getFieldValue(colName, docName, 'field') == ['val1', 'val2']
+
+def testCase_6():
+  try:
+    firestoreStorage.updateField(colName, docName, [('field', ['??', 'val2'])])
+  except Exception:
+    return True
+
+def testCase_7():
+  try:
+    firestoreStorage.updateField(colName, docName, [('field', ['', '', ''])])
+  except Exception:
+    return True
